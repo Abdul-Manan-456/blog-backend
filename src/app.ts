0f3dce7369ef42ab0b36db8bd9fd2a925b8b1449
abdul-manan-456 <abdulmanan777333@gmail.com>
2024-05-27 15:14:13 +0500

    changes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import indexRouter from "./routes";
 import { errorHandler } from "./utils";
 config();
 const app: Express = express();
-const file = "abc";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
